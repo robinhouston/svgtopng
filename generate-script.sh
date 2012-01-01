4be314a0f703ae28cac6acf7ded5c9dc765ea6f0
Robin Houston <robin.houston@gmail.com>
2012-01-01 19:19:32 +0000

    Increase the max heap size to 1G, and use AWT in headless mode

--- generate-script.sh
+++ generate-script.sh
@@ -9,7 +9,7 @@ echo "Generating $1..."
 cat > "$1" <<CAT
 #!/bin/bash
 export CLASSPATH="${BATIK_JARS}:${INSTALLDIR}"
-java SVGToPNG "\$0" "\$@"
+java -Xmx1G -Djava.awt.headless=true SVGToPNG "\$0" "\$@"
 CAT
 
 chmod 755 "$1"
